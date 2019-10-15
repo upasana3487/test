43bee6e10803252840c3dbc394043017ebd9a7bd
upasana3487 <upasana3487@gmail.com>
2019-10-15 13:53:30 -0400

    Add files via upload

---
+++ FileReadDemo.java
@@ -0,0 +1,26 @@
+package Exception;
+
+import java.io.*;
+
+public class FileReadDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		File f = new File ("\upasana.txt");
+		FileReader fr = new FileReader(f);
+		int ch = fr.read();
+		while(ch!=-1)
+		{
+		System.out.println((char)ch);
+		ch=fr.read();
+		}
+		System.out.println(f.canRead());
+		System.out.println(f.canWrite());
+
+		fr.close();
+		
+
+	}
+
+}
